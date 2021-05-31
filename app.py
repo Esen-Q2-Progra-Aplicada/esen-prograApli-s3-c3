f9896569b99f2b2e531a1a85f1b0616f2f565f05
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-05-30 18:44:30 -0600

    modify app, index, register

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from logic.user_logic import UserLogic
+import bcrypt
 
 app = Flask(__name__)
 
@@ -8,10 +10,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
+    elif request.method == "POST":
+        logic = UserLogic()
+        userName = request.form["username"]
+        userEmail = request.form["useremail"]
+        passwd = request.form["passwd"]
+        confpasswd = request.form["confpasswd"]
+        # logic.insertUser()
+        return "posted"
 
 
 if __name__ == "__main__":
